10635984173653a3bc63514cf4fa5853089e3a1a
bernardoholtz <bernardoholtz@gmail.com>
2017-08-08 01:17:03 -0300

    melhorias layout

--- desafiohu/app/public/js/front.js
+++ desafiohu/app/public/js/front.js
@@ -1,5 +1,5 @@
 
-var hotel_sel
+
 var app = angular.module('ofertasApp', ['ui.bootstrap']);
 
 
@@ -14,23 +14,16 @@ app.controller('CtrlOferta',function($scope,$http){
 		
 		$http.get('/api/oferta')
             .success(function(data) {
-				   hotel_sel = [];
+				  var hotel_sel = [];
 				  hotel_sel = data;
 				  $scope.id = hotel_sel.id;
 				  $scope.nome = hotel_sel.title;
 				  $scope.local = hotel_sel.location;
 				  $scope.descricao = hotel_sel.description;
 				  hotel_sel.photos.forEach(function(foto){ $scope.slides.push({'image':foto}) });
-				  jQuery(document).ready(function () {
-					  if (hotel_sel.photos.length == 5){
-						  $('.sl').addClass('tam5');
-					  }else if (hotel_sel.photos.length == 8){
-						  $('.sl').addClass('tam8');
-					  };
-				  });
 				  $scope.opcoes = hotel_sel.options; 
-				  $scope.diarias = hotel_sel.diarias;	
-                  $scope.saidas = hotel_sel.saidas;
+			
+                
             })
             .error(function(data) {
                 alert('Error: ' + data);
@@ -50,7 +43,7 @@ app.controller('CtrlOferta',function($scope,$http){
 				  var hotel_sel = [];
 				  hotel_sel = data;
 				  $scope.opcoes = hotel_sel.options; 
-				  $scope.diarias = hotel_sel.diarias;	
+			
                 
             })
             .error(function(data) {
@@ -63,7 +56,9 @@ app.controller('CtrlOferta',function($scope,$http){
 
 
   $scope.myInterval = 5000;
-    
+  
+  $scope.saidas = ["Belém","Boa Vista","Brasilia","Campo Grande","Cuiabá","Ilhéus","Manaus","Rio de Janeiro","Salvador","São Paulo"];
+  $scope.diarias = ["1","2","3","4","5","6","7","8","9","10","11","12"];
 
   $scope.destacarFoto = function(id){
   	jQuery(document).ready(function () {
--- desafiohu/app/routes.js
+++ desafiohu/app/routes.js
@@ -8,23 +8,37 @@ module.exports = function (app) {
 		var hotel =[];
 		
 			
-		arquivo.readFile(__dirname + '/data/offer.txt',function(err,file){
+		arquivo.readFile(__dirname + '/data/offer.txt','utf-8',function(err,file){
 			if(err){
 				res.send(err);
 			}else{
 				var obj = JSON.parse(file);
 				
-				//var id = Math.floor(Math.random() * 12);
-				var id = 6;
+				var id = Math.floor(Math.random() * 12);
+				//var id = 8;
 				
 				for (var i=0; i<obj.length; i++){
 					if (obj[i].id == id) {
 						hotel = obj[i];
 						hotel.options.sort(function(a,b) {return (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0);} ); 
+						
+						hotel.diarias = [];
+						hotel.options.forEach(function(op){hotel.diarias.push(op.daily);});
+						hotel.diarias = hotel.diarias.filter(function(elem, index, self) { return index == self.indexOf(elem);})
+						hotel.diarias.sort(function(a,b) {return (a > b) ? 1 : ((b > a) ? -1 : 0);} ); 
+						
+						hotel.saidas = [];
+						for (var i=0;i<hotel.options.length;i++){
+							hotel.options[i].from.forEach(function(op){hotel.saidas.push(op);});
+							hotel.options[i].from.sort(function(a,b) {return (a > b) ? 1 : ((b > a) ? -1 : 0);} ); 
+						}
+						hotel.saidas = hotel.saidas.filter(function(elem, index, self) { return index == self.indexOf(elem);})
+						hotel.saidas.sort(function(a,b) {return (a > b) ? 1 : ((b > a) ? -1 : 0);} ); 
+						
+						
 						res.header("Content-Type", "application/json; charset=utf-8");
 						res.send(JSON.stringify(hotel));
-						
-						
+						break;						
 					}
 				}
 
@@ -60,12 +74,18 @@ module.exports = function (app) {
 							hotel.options = hotel.options.map(function(op){ if (op.daily == req.body[2]) return op});
 							hotel.options= hotel.options.filter(function( element ) { return element !== undefined;});
 						};
+						hotel.diarias = [];
 						if (req.body[1] != "") {
 							var options = [];
+							hotel.diarias =[];
 							for (var j=0; j<hotel.options.length; j++){
 								for (var x=0; x<hotel.options[j].from.length;x++){
-									if (hotel.options[j].from[x] == req.body[1])
+									if (hotel.options[j].from[x] == req.body[1]){
+										hotel.diarias.push(hotel.options[j].daily);
 										options.push(hotel.options[j]);
+										
+									}
+										
 									
 								};
 							};
@@ -73,14 +93,24 @@ module.exports = function (app) {
 						};
 												
 						hotel.options.sort(function(a,b) {return (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0);} ); 
+						ordenaSaida();
+						ordenaDiaria();
 						res.header("Content-Type", "application/json; charset=utf-8");
 						res.send(JSON.stringify(hotel));
+						break;
 										
 					}
 				}
 				
-				function buscaDiaria(value) {
-				  return value == parseInt(req.body[2]);
+				function ordenaSaida() {
+				  for (var i=0;i<hotel.options.length;i++){
+						hotel.options[i].from.sort(function(a,b) {return (a > b) ? 1 : ((b > a) ? -1 : 0);} ); 
+					}
+				}
+				
+				function ordenaDiaria(){
+					hotel.diarias = hotel.diarias.filter(function(elem, index, self) { return index == self.indexOf(elem);})
+					hotel.diarias.sort(function(a,b) {return (a > b) ? 1 : ((b > a) ? -1 : 0);} ); 
 				}
 
 			}
